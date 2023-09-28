7d1271fb060f960b8411bc32ff107e4ffee52b2e
gbiagi <geanfrancobiagi@hotmail.com>
2023-09-28 23:14:32 +0200

    Correciones y acabado Pr1.2

--- src/Pr12LecturaEscritura/Main.java
+++ src/Pr12LecturaEscritura/Main.java
@@ -1,26 +1,34 @@
 package Pr12LecturaEscritura;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 
 public class Main {
     static Scanner in = new Scanner(System.in); // System.in és global
-
-
     // Main
     public static void main(String[] args) throws InterruptedException, IOException {
         boolean running = true;
+        // Crear carpeta para los archivos por si se elimina
+        File dirName = new File("./src/Pr12LecturaEscritura/Archivos");
+        // Crear carpeta
+        if (!dirName.exists()){
+            if(!dirName.mkdirs()) {
+                System.out.println("Error en la creació de la carpeta '" + dirName + "'");
+            } else {
+                System.out.println("Carpeta creada");
+            }
+        }
         while (running) {
             String menu = "Escull una opció:";
             menu = menu + "\n 0) PR120ReadFile";
             menu = menu + "\n 1) PR121Files";
-            menu = menu + "\n 2) Pr12LecturaEscritura.PR122cat";
-            menu = menu + "\n 3) Pr123 Crear arxiu";
-            menu = menu + "\n 4) PR123Append";
-            menu = menu + "\n 5) PR123Sobreescriu";
-            menu = menu + "\n 6) PR124Linies";
-            menu = menu + "\n 7) PR125Copy File";
+            menu = menu + "\n 2) PR122cat";
+            menu = menu + "\n 3) Pr123append";
+            menu = menu + "\n 4) PR123Sobreescriu";
+            menu = menu + "\n 5) PR124Linies";
+            menu = menu + "\n 6) PR125Copy File";
             menu = menu + "\n 100) Sortir";
             System.out.println(menu);
 
@@ -30,12 +38,11 @@ public class Main {
                     case 0: PR120ReadFile.main(args); break;
                     case 1: PR121Files.main(args); break;
                     case 2: PR122cat.main(llegirLinia("Ruta del arxiu: ")); break;
-                    case 3: Pr123.main(args); break;
-                    case 4: PR123append.main(args); break;
-                    case 5: PR123sobreescriu.main(args); break;
-                    case 6: PR124linies.main(args); break;
-                    case 7: PR125cp.main(llegirLinia("Ruta archiu a copiar: "), llegirLinia("Ruta destinacio: ")); break;
-                    case 8: llegirLinia("Hola David");
+                    case 3: PR123append.main(args); break;
+                    case 4: PR123sobreescriu.main(args); break;
+                    case 5: PR124linies.main(args); break;
+                    case 6: PR125cp.main(llegirLinia("Ruta archiu a copiar: "), llegirLinia("Ruta destinacio: ")); break;
+                    case 7: llegirLinia("Hola David");
                     case 100: running = false; break;
                     default: break;
                 }
--- src/Pr12LecturaEscritura/PR121Files.java
+++ src/Pr12LecturaEscritura/PR121Files.java
@@ -2,6 +2,7 @@ package Pr12LecturaEscritura;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class PR121Files {
     public static void main(String[] args) {
@@ -9,13 +10,18 @@ public class PR121Files {
         File dirName = new File(dir + "myFiles");
         File file1 = new File(dirName + "/" + "file1.txt");
         File file2 = new File(dirName + "/" + "file2.txt");
+        Scanner scnr = new Scanner(System.in);
 
          // Crear carpeta
         if (!dirName.exists()){
             if(!dirName.mkdirs()) {
                 System.out.println("Error en la creació de la carpeta '" + dirName + "'");
+            } else {
+                System.out.println("Carpeta creada");
             }
         }
+        System.out.println("Presiona Enter per continuar");
+        scnr.nextLine();
         // Crear archivos
         try {
             if (file1.createNewFile()) {
@@ -30,6 +36,8 @@ public class PR121Files {
                 System.out.println("File already exists.");
             }
         } catch (IOException e) { e.printStackTrace(); }
+        System.out.println("Presiona Enter per continuar");
+        scnr.nextLine();
 
         // Renombra archivo “file2.txt” a “renamedFile.txt”.
         if (file2.renameTo(new File(dirName + "/" + "renamedFile.txt"))){
@@ -37,6 +45,9 @@ public class PR121Files {
         } else {
             System.out.println("Failed rename\n");
         }
+        System.out.println("Presiona Enter per continuar");
+        scnr.nextLine();
+
         // Muestra un listado de los archivos dentro de la carpeta "myfiles" con el mensaje "Els arxius de la carpeta son:"
         try {
             File[] filesList = dirName.listFiles();
@@ -45,12 +56,18 @@ public class PR121Files {
                 System.out.println("Archiu: " + file.getName());
             }
             } catch (Exception e) {e.printStackTrace();}
+        System.out.println("Presiona Enter per continuar");
+        scnr.nextLine();
+
         // Elimina “file1.txt”.
             if (file1.delete()) {
                 System.out.println(file1.getName() + " deleted");
             } else {
                 System.out.println("Failed delete of file.");
             }
+        System.out.println("Presiona Enter per continuar");
+        scnr.nextLine();
+
         // Vuelve a mostrar un listado de los archivos de la carpeta "myfiles"
         try {
             File[] filesList = dirName.listFiles();
