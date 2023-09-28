2ae2dee24e6aece579b9f73b32287e6da95d5e52
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2023-09-28 17:10:51 +0200

    update

--- src/Pr12LecturaEscritura/PR125cp.java
+++ src/Pr12LecturaEscritura/PR125cp.java
@@ -1,5 +1,35 @@
 package Pr12LecturaEscritura;
 
-public class PR125cp {
+import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class PR125cp {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        Scanner readFile;
+        String pathSource;
+        String pathTarget;
+
+        try {
+            System.out.println("Ruta archivo: ");
+            pathSource = scnr.nextLine();
+            System.out.println("Ruta de destinacio: ");
+            pathTarget = scnr.nextLine();
+            scnr.close();
+            //File copiedFile = new File(pathTarget);
+            FileWriter copiedFile = new FileWriter(pathTarget);
+            readFile = new Scanner(pathSource);
+            while (readFile.hasNextInt()) {
+                String line = readFile.nextLine();
+                copiedFile.write(line);
+            }
+            copiedFile.close();
+            readFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
