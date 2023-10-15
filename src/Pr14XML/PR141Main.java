a25702a6083af2942e2b1a5cb166522164cf2416
gbiagi <geanfrancobiagi@hotmail.com>
2023-10-15 22:50:36 +0200

    Update

--- src/Pr14XML/PR141Main.java
+++ src/Pr14XML/PR141Main.java
@@ -28,44 +28,44 @@ public class PR141Main {
             // Crear element principal
             Element elmPrincipal = doc.createElement("llibre");
             // Crear atributo
-            //Attr atrbID = doc.createAttribute("001");
             elmPrincipal.setAttribute("id", "001");
             elmRoot.appendChild(elmPrincipal);
 
             // Crear elementos + la informacion
+            Text nodeText;
             Element elmTitulo = doc.createElement("titol");
-            Text nodeText1 = doc.createTextNode("El viatge del venturons");
-            elmTitulo.appendChild(nodeText1);
+            nodeText = doc.createTextNode("El viatge del venturons");
+            elmTitulo.appendChild(nodeText);
             elmPrincipal.appendChild(elmTitulo);
 
             Element elmAutor = doc.createElement("autor");
-            Text nodeText2 = doc.createTextNode("Joan Pla");
-            elmAutor.appendChild(nodeText2);
+            nodeText = doc.createTextNode("Joan Pla");
+            elmAutor.appendChild(nodeText);
             elmPrincipal.appendChild(elmAutor);
 
             Element elmAny = doc.createElement("anyPublicacio");
-            Text nodeText3 = doc.createTextNode("1998");
-            elmAny.appendChild(nodeText3);
+            nodeText = doc.createTextNode("1998");
+            elmAny.appendChild(nodeText);
             elmPrincipal.appendChild(elmAny);
 
             Element elmEditorial = doc.createElement("editorial");
-            Text nodeText4 = doc.createTextNode("Edicions Mar");
-            elmEditorial.appendChild(nodeText4);
+            nodeText = doc.createTextNode("Edicions Mar");
+            elmEditorial.appendChild(nodeText);
             elmPrincipal.appendChild(elmEditorial);
 
             Element elmGenere = doc.createElement("genere");
-            Text nodeText5 = doc.createTextNode("Aventura");
-            elmGenere.appendChild(nodeText5);
+            nodeText = doc.createTextNode("Aventura");
+            elmGenere.appendChild(nodeText);
             elmPrincipal.appendChild(elmGenere);
 
             Element elmPaginas = doc.createElement("pagines");
-            Text nodeText6 = doc.createTextNode("320");
-            elmPaginas.appendChild(nodeText6);
+            nodeText = doc.createTextNode("320");
+            elmPaginas.appendChild(nodeText);
             elmPrincipal.appendChild(elmPaginas);
 
             Element elmDisponible = doc.createElement("disponible");
-            Text nodeText7 = doc.createTextNode("true");
-            elmDisponible.appendChild(nodeText7);
+            nodeText = doc.createTextNode("true");
+            elmDisponible.appendChild(nodeText);
             elmPrincipal.appendChild(elmDisponible);
 
             // Guardar datos en un xml
@@ -76,5 +76,7 @@ public class PR141Main {
             DOMSource source = new DOMSource(doc);
             StreamResult result = new StreamResult(new File("./src/Pr14XML/Archivos/biblioteca.xml"));
             transformer.transform(source, result);
+
         } catch (Exception e) {e.printStackTrace();}
-    }}
+    }
+}
---
+++ src/Pr14XML/Pr142Main.java
@@ -0,0 +1,5 @@
+package Pr14XML;
+
+public class Pr142Main {
+
+}
