71edda94581b2b45298bf2ef59915f866dde571a
gbiagi <geanfrancobiagi@hotmail.com>
2023-10-19 23:30:00 +0200

    PR142 end

--- src/Pr14XML/Pr142Main.java
+++ src/Pr14XML/Pr142Main.java
@@ -6,9 +6,14 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -46,9 +51,15 @@ public class Pr142Main {
                         case 1:
                             mostrarPorID(doc, xPath);
                             break;
-                        case 2: break;
-                        case 3: break;
-                        case 4: break;
+                        case 2:
+                            listarAlumnes(doc, xPath);
+                            break;
+                        case 3:
+                            afegirAlumne(doc, xPath);
+                            break;
+                        case 4:
+                            eliminarAlumne(doc, xPath);
+                            break;
                         case 100:
                             running = false;
                             break;
@@ -94,6 +105,70 @@ public class Pr142Main {
                     System.out.println("ID del mòdul: " + modulID + ", Títol del mòdul: " + modulTitle);
                 }
             } else {System.out.println("El id no existeix.\n");}
+        } catch (Exception e) {e.printStackTrace();}
+    }
+    public static void listarAlumnes(Document doc, XPath xPath) {
+        String id = llegirLinia("ID del curs: ");
+        String expression = "/cursos/curs[@id='" + id + "']/alumnes/alumne";
+        try {
+            NodeList listaAlumnes = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+            if (listaAlumnes.getLength() > 0) {
+                for (int i = 0; i < listaAlumnes.getLength(); i++) {
+                    String studentName = listaAlumnes.item(i).getTextContent();
+                    System.out.println("Nom: " + studentName);
+                }
+            } else {System.out.println("El ID no existeix.\n");}
+        } catch (Exception e) {e.printStackTrace();}
+    }
+    public static void afegirAlumne(Document doc, XPath xPath) {
+        String id = llegirLinia("ID del curs: ");
+        String expression = "/cursos/curs[@id='" + id + "']/alumnes";
+        try {
+            NodeList listaCursos = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+            if (listaCursos.getLength() > 0) {
+                String nomAlumne = llegirLinia("Nom de l'alumne: ");
+
+                // Crear el alumno en XML
+                Element alumnosElement = (Element) listaCursos.item(0);
+                Element nuevoAlumnoElement = doc.createElement("alumne");
+                nuevoAlumnoElement.setTextContent(nomAlumne);
+                alumnosElement.appendChild(nuevoAlumnoElement);
+
+                // Guardar el XML
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File("./src/Pr14XML/Archivos/cursos.xml"));
+                transformer.transform(source, result);
+
+                System.out.println("Alumne afegit al curs.\n");
+
+            } else {System.out.println("El curs no existeix.\n");}
+        } catch (Exception e) {e.printStackTrace();}
+    }
+    public static void eliminarAlumne(Document doc, XPath xPath) {
+        String id = llegirLinia("ID del curs: ");
+        String nomAlumne = llegirLinia("Nom de l'alumne: ");
+
+        String expression = "/cursos/curs[@id='" + id + "']/alumnes/alumne[text()='" + nomAlumne + "']";
+        try {
+            NodeList listaAlumnes = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+            if (listaAlumnes.getLength() > 0) {
+
+                // Eliminar al alumno del XML
+                Element alumnoElement = (Element) listaAlumnes.item(0);
+                alumnoElement.getParentNode().removeChild(alumnoElement);
+
+                // Guardar el XML modificado
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File("./src/Pr14XML/Archivos/cursos.xml"));
+                transformer.transform(source, result);
+
+                System.out.println("Alumne elimnat del curs.\n");
+
+            } else {System.out.println("No s'ha trobat l'alumne.\n");}
         } catch (Exception e) {e.printStackTrace();}
     }
 }
